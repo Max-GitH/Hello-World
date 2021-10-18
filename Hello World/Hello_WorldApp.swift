02878f70f61e8b08e467684e4e77ab4391f07b30
Max <mnonisa3@getech.edu>
2021-10-18 12:27:03 -0700

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Max Nonisa on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
